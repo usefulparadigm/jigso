073b4f00a02cb6e307de3f5cde29c4ac72add5e5
Sukjoon Kim <sjoonk@gmail.com>
2011-09-09 12:34:41 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ultrabase::Application.config.secret_token = 'bb5fe767c735fb45e579189c0ce7ea4b95c8a8117fadab9ea52ddbff7bb684066acd544781938930af10b6b1df31326fb3204f3e13ea2fa366a70a693f0122f1'
