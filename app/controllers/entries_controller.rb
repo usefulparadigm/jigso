fe5e94685b2c4e85b12c7a179b452ce1472e300d
Sukjoon Kim <sjoonk@gmail.com>
2011-09-14 17:09:59 +0900

    disable cancan

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,7 +2,7 @@ class EntriesController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
   respond_to :html, :json
   inherit_resources
-  load_and_authorize_resource
+  # load_and_authorize_resource
 
   def index
     respond_with(@entries = Entry.order('created_at DESC').page(params[:page]))
