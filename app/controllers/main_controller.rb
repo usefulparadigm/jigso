3618fd3116e9724cb588359a55555ab089d04d93
Sukjoon Kim <sjoonk@gmail.com>
2011-09-28 14:58:47 +0900

    add recent activities stream on main index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,9 @@
 class MainController < ApplicationController
-  skip_before_filter :authenticate_user! #, :except => [:index, :show]
+  skip_before_filter :authenticate_user!
 
   def index
-    redirect_to home_path if current_user
+    # redirect_to home_path if current_user
+    @activities = TimelineEvent.limit(10)
   end
 
 end
